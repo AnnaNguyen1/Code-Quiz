9c47eaa00846e9c6fda41e6c888300b6a488f24b
Anna <an9062@gmail.com>
2021-09-12 17:55:32 +1000

    added endGame function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,7 @@
 var timerCount = document.querySelector("#timer-text");
 var startButton = document.querySelector("#start-button");
-var header2 = document.querySelector(".header-2");
+var header = document.querySelector(".header");
+var displayQuestionNumber = document.querySelector(".question-number");
 var displayQuestionText = document.querySelector(".question");
 var displayAnswersText = document.querySelector(".buttons-container");
 var displayResult = document.querySelector(".result");
@@ -42,6 +43,7 @@ var questions =
 
 // Setting default values
 var score = 0;
+var numberQuestionsCorrect = 0;
 var currentQuestionIndex = -1;
 var timeLeft = 76;
 var penaltyTime = 15;
@@ -56,13 +58,16 @@ function startTimer() {
     timerCount.textContent = timeLeft; // Display timeLeft = 76
 
     countDown = setInterval(function() { 
+        timeLeft--;
+        timerCount.textContent = timeLeft;
+
         if (timeLeft <= 0) { // Stop game when time runs out
             clearInterval(countDown);
+            timerCount.textContent = 0;
             endGame();
             return;
         }
-        timeLeft--;
-        timerCount.textContent = timeLeft;
+        
     }, 1000);
 };
 
@@ -78,11 +83,15 @@ startButton.addEventListener("click", function() {
 )
 // Display questions 
 function displayQuestion() {
-    // Clear values to start off
-    header2.textContent = "";
+    // Hide header
+    // header.textContent = "";
 
     // Start off at 0
     currentQuestionIndex++;
+
+    // Display Q number
+    displayQuestionNumber.textContent = "Question " + (currentQuestionIndex + 1);
+    displayQuestionNumber.style.textAlign = "left";
 
     // Set variables for the question, choices and answers
     var question = questions[currentQuestionIndex].ask;
@@ -97,30 +106,50 @@ function displayQuestion() {
     // Display question according to index
     var currentQuestion = question;
     displayQuestionText.textContent = currentQuestion;
+    displayQuestionText.style.textAlign = "left";
+
+    // Clear existing answer values
+    displayAnswersText.innerHTML = [];
     
     // For loop over the answer buttons
     for (var i=0; i < options.length; i++ ) {
         var buttonAnswer = document.createElement("button");
         buttonAnswer.setAttribute("class", "btn-options")
-        buttonAnswer.textContent = (i + 1) + "." + options[i];
+        buttonAnswer.textContent = options[i];
         displayAnswersText.appendChild(buttonAnswer);
         buttonAnswer.addEventListener('click', checkAnswer);
+
     }
 
 }
 
 // Answer click function 
 function checkAnswer(event) {
+    var correct = "Correct!";
+    var incorrect = "Incorrect!";
+
     event.preventDefault();
     var element = event.target; // event.target refers to the clicked event 
-    if (element.textContent === questions[currentQuestionIndex].answer) {
-        displayResult.textContent = "Correct!"
-        score += correctScore;
-    } else 
-        displayResult.textContent = "Incorrect!"
-        score -= incorrectScore;
 
-    if (currentQuestionIndex === questions.length) {
+    if (element.textContent === questions[currentQuestionIndex].answer) {
+        displayResult.textContent = correct;
+        score += correctScore;
+        numberQuestionsCorrect++;
+        console.log(numberQuestionsCorrect);
+        console.log(score);
+    } else 
+        displayResult.textContent = incorrect;
+        score -= incorrectScore;
+        timeLeft -= penaltyTime;
+        console.log(numberQuestionsCorrect);
+        console.log(score);
+
+    // Remove text after 1 second
+    setTimeout(function() {
+        displayResult.textContent = "";
+    }, 1000);
+
+    if (currentQuestionIndex === (questions.length + 1)) {
         endGame();
     } else
         displayQuestion();
@@ -129,5 +158,15 @@ function checkAnswer(event) {
 
 // End Game
 function endGame() {
+    var header = "";
+    var displayQuestionNumber = "";
+    var displayQuestionText = "";
 
+    if (score < 0) {
+        score.textContent = 0;
+    }
+
+    header.textContent = "End of Quiz!";
+    displayQuestionNumber.textContent = "You got " + numberQuestionsCorrect + "questions correct.";
+    displayQuestionText.textContent = "Your final score is " + score + " .";
 }
